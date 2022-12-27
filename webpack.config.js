ca6696ca5f967878282842914fe6f20fe822aeca
alex2075 <gagara5620@gmail.com>
2022-12-27 19:29:51 +0600

    feat: forkts not args

--- webpack.config.js
+++ webpack.config.js
@@ -211,22 +211,24 @@ module.exports = {
             minifyJS: true,
           },
     }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      logger: {
-        infrastructure: 'silent',
-        issues: 'webpack-infrastructure',
-        devServer: true,
-      },
-      typescript: {
-        configFile: '../tsconfig.json',
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true,
-        },
-        mode: 'write-references',
-      },
-    }),
+    new ForkTsCheckerWebpackPlugin(
+    //   {
+    //   async: false,
+    //   logger: {
+    //     infrastructure: 'silent',
+    //     issues: 'webpack-infrastructure',
+    //     devServer: true,
+    //   },
+    //   typescript: {
+    //     configFile: '../tsconfig.json',
+    //     diagnosticOptions: {
+    //       semantic: true,
+    //       syntactic: true,
+    //     },
+    //     mode: 'write-references',
+    //   },
+    // }
+    ),
     new CopyPlugin({
       patterns: [
         {
